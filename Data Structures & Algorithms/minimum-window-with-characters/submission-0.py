e688231e8715e7903c69e05dc31f2d8c27a6b3da
Tushar B kumar <67858589+tbk07@users.noreply.github.com>
2026-04-16 15:06:22 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        dict1 = {}
+        dict2 = {}
+        for i in range(len(t)) :
+            dict1[t[i]] = dict1.get(t[i],0) +1
+        for i in range(len(s)):
+            dict2[s[i]] = dict2.get(s[i],0) + 1
+        is_subset = dict2.items() <= dict1.items()
+        if is_subset is False:
+            return ""
+        
+                
+
+
+
+
+
+
+
+            
+ 
